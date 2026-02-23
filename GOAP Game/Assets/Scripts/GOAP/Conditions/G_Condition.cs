b0ee3084d57103976e0da940d602fc82e549c3a7
GebwellB <170325747+GebwellB@users.noreply.github.com>
2026-02-23 13:11:14 +0800

    Worked through Tom's videos, up to 'Session 1, 4a. Actions'

---
+++ GOAP Game/Assets/Scripts/GOAP/Conditions/G_Condition.cs
@@ -0,0 +1,121 @@
+using UnityEngine;
+
+namespace GOAP
+{
+    public class G_Condition
+    {
+        #region Variables
+        // The state being tested by the condition
+        G_State state;
+        public G_State State
+        {
+            get { return state; }
+        }
+
+        // The actual comparison for the condition
+        G_StateComparison comparison;
+        public G_StateComparison Comparison
+        {
+            get { return comparison; }
+        }
+
+        // The value we will be comparing to the current value in the state
+        object expectedValue;
+        public object ExpectedValue
+        {
+            get { return expectedValue; }
+        }
+
+        // Has the coniditon been met during planning?
+        bool met = false;
+        public bool Met
+        {
+            get { return met; }
+        }
+        #endregion
+
+        public G_Condition(G_State state, object expectedValue, G_StateComparison comparison = G_StateComparison.equal, bool met = false)
+        {
+            this.state = state;
+            this.expectedValue = expectedValue;
+            this.comparison = comparison;
+            this.met = met;
+        }
+
+
+        #region Functions
+
+        /// <summary>
+        /// Returns true if this condition matches the effect condition parameter
+        /// </summary>
+        /// <param name="effect"></param>
+        /// <returns></returns>
+        public bool CompareConditionTooEffect(G_Condition effect)
+        {
+            if (state.TestValueMatch(effect.state.GetValue()))
+            {
+                return state.TestStateConditionMatch(this, effect);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Returns true if the condition defined in this condition instance is actually correct
+        /// </summary>
+        /// <returns></returns>
+        public bool DoesStateMeetCondition()
+        {
+            return state.TestState(state, comparison, expectedValue);
+        }
+
+        /// <summary>
+        /// Returns true if the condition defined in this condition instance is
+        /// actually correct when applied to an external state
+        /// </summary>
+        /// <param name="stateToTest"></param>
+        /// <returns></returns>
+        public bool DoesStateMeetCondition(G_State stateToTest)
+        {
+            bool success = false;
+
+            if (state.TestValueMatch(stateToTest.GetValue()))
+            {
+                success = state.TestState(stateToTest, comparison, expectedValue);
+            }
+            else
+            {
+                Debug.LogWarning("Value type of state to test did not match internal state");
+            }
+
+            return success;
+        }
+
+        /// <summary>
+        /// Returns true if the state to compare has the same name as the state in this condition
+        /// </summary>
+        /// <param name="stateToCompare"></param>
+        /// <returns></returns>
+        public bool IsStateTheConditionState(G_State stateToCompare)
+        {
+            return stateToCompare.name == state.name;
+        }
+
+        /// <summary>
+        /// Sets the variable met to true. Used during planning
+        /// </summary>
+        public void Meet()
+        {
+            met = true;
+        }
+
+        public static G_Condition Clone(G_Condition conditionToClone)
+        {
+            return new G_Condition(conditionToClone.state, conditionToClone.expectedValue, conditionToClone.comparison, conditionToClone.met);
+        }
+
+        #endregion
+    }
+}
---
+++ GOAP Game/Assets/Scripts/GOAP/States/G_State.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using GOAP;
+
+namespace GOAP
+{
+    public class G_State : ScriptableObject
+    {
+        // The value we are storing
+        object value;
+
+        #region Basic Controls
+        public virtual void Construct(string name, object value)
+        {
+            this.name = name;
+            SetValue(value);
+        }
+
+        public virtual void SetValue(object value)
+        {
+            this.value = value;
+        }
+
+        public virtual object GetValue()
+        {
+            return value;
+        }
+
+        public virtual G_State Clone()
+        {
+            G_State clone = ScriptableObject.CreateInstance<G_State>();
+            clone.Construct(this.name, this.value);
+            return clone;
+        }
+
+        #endregion
+
+        #region Testing Controls
+        /// <summary>
+        /// Tests the given state agaisnt the expectedValue using the chosen comparison, returning true if the comparison
+        /// is correct and false if not
+        /// </summary>
+        /// <param name="state"></param>
+        /// <param name="expectedValue"></param>
+        /// <returns></returns>
+        public virtual bool TestState(G_State state, G_StateComparison comparison, object expectedValue)
+        {
+            Debug.LogWarning($"Base class G_State has no State testing implemented - returning false for {state.name}");
+            return false;
+        }
+
+        /// <summary>
+        /// This function returns true if the two given conditions match their states, expected values, and comparisons
+        /// </summary>
+        /// <returns></returns>
+        public virtual bool TestStateConditionMatch(G_Condition precondition, G_Condition effect)
+        {
+            Debug.LogWarning($"Base class G_State has no condition comparisons implemented - returning false");
+            return false;
+        }
+
+        /// <summary>
+        /// Returns true if the state type has an implementation for the given comparison type
+        /// </summary>
+        /// <returns></returns>
+        public virtual bool StateSupportsComparion(G_StateComparison comparison)
+        {
+            Debug.LogWarning($"Base class G_State doesn't support any comparisons - returning false");
+            return false;
+        }
+
+        /// <summary>
+        /// Tests if the given value is of the same type as the value stored in this state and returns true if it is
+        /// </summary>
+        /// <param name="testValue"></param>
+        /// <returns></returns>
+        public virtual bool TestValueMatch(object testValue)
+        {
+            Debug.LogWarning($"Base class G_State doesn't support value matching - returning false");
+            return false;
+        }
+
+        #endregion
+    }
+}
